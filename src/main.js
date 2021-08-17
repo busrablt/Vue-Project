1f10eb07658f95ea0e52a0ca09a4d1be3b4f89d2
Muhammet BİLGİN <muhammetbilginn@gmail.com>
2021-08-17 16:35:15 +0300

    main.js updated

--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from './plugins/vuetify'
+import axios from "axios"
+import firebase from "firebase/app"
 
+Vue.prototype.$axios=axios;
 Vue.config.productionTip = false;
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDH1LjLqEAH-kF0pT7mcTPtUSFr4WdoP4c",
+    authDomain: "vuelogin-903ec.firebaseapp.com",
+    projectId: "vuelogin-903ec",
+    storageBucket: "vuelogin-903ec.appspot.com",
+    messagingSenderId: "9569109473",
+    appId: "1:9569109473:web:621952eff2b400c324bea0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
