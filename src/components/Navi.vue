88ec9a76dea56740372bf4a09060bd2bf8c9567b
Muhammet BİLGİN <muhammetbilginn@gmail.com>
2021-08-23 16:32:03 +0300

    Navi component refactored

--- src/components/Navi.vue
+++ src/components/Navi.vue
@@ -1,46 +1,34 @@
 <template>
-  <v-app-bar
-      app
-      color="white"
-      flat
-      
-    >
-      <v-container class="py-0 fill-height header__sheet" >
-        <div class="header__avatar">
-          <v-avatar
-            class="mr-10"
-            color="grey darken-1"
-            size="32"
-          ></v-avatar>
-        </div>
-        <div class="header__btn">
-          <v-btn
-            v-for="link in links"
-            :key="link"
-            text
-          >
-            {{ link }}
-          </v-btn>
-        </div>
-       
-      </v-container>
-    </v-app-bar>
+  <v-app-bar app color="white" flat>
+    <v-container class="py-0 fill-height header__sheet">
+      <div>
+        <img src="../assets/mobven.png" alt="Logo" width="150" height="50" >
+      </div>
+      <div class="information">
+        <v-icon class="information__icon" size="20">mdi-help-circle-outline</v-icon>
+        <v-icon class="information__icon" size="20">mdi-bell-outline</v-icon>
+        <h class="information__icon">Deneme</h>
+        <v-avatar class="mr-10 information__icon" color="grey darken-1" size="32"></v-avatar>
+      </div>
+    </v-container>
+  </v-app-bar>
 </template>
-
 <script>
 export default {
-    name:"Navi",
-    data: () => ({
-      links: [ 
-        'Profile',
-        'Updates',
-      ],
-     
-    }),
-
-}
+  name: "Navi",
+  data: () => ({
+    links: ["Profile", "Updates"],
+  }),
+};
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+.information {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  &__icon{
+    margin: 0.4em;
+  }
+}
 </style>
